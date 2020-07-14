b0e73752884129a960894d8ee3b823a0020c4434
Tom Overland <tomoverland03@gmail.com>
2020-07-14 10:26:06 -0500

    made changes to script.js

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Assigning a length to the password from a prompt
+var passwordLength = prompt('What would you like the length of the password to be? (Please enter a number between 8 and 120)');
+
+// Assigning character types to password input
+var characterTypes = confirm('Would you like to use lowercase letters in your password?  Please select "OK" for yes, and "cancel" for no.')
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword(); {
+
+  }
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);{
+    passwordLength;
+    //console.log("The password length will be " + passwordLength + " characters long");
+
+
+}
 
