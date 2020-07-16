7ce8e82cce37bd1e9f7fce86b24292b8efc651ea
Tom Overland <tomoverland03@gmail.com>
2020-07-16 14:09:57 -0500

    making changes to script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Start Working Code
 // User Input Variables
-var enter;
+var passwordLength;
 var confirmNumber;
 var confirmCharacter;
 var confirmUppercase;
@@ -23,8 +23,92 @@ var generateBtn = document.querySelector("#generate");
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword); {
-   
+// ---PASSWORD---
+// Write password to the #password input
+function writePassword() {
+    var passwordText = document.querySelector("#password");
+    var password = generatePassword(); {
+    passwordText.value = password;
+    }
 };
+   // consider deleting // ps = generatePassword();
+   // document.getElementById("password").placeholder = ps;
+};
+
+function generatePassword() {
+    //Asks for User Input
+    passwordLength = parseInt(prompt("How many characters long would you like your password to be?  Please enter an amount between 8 and 128."));
+    //If statement for user validation, first statement is if the field is left blank
+    if (!passwordLength) {
+        alert("You must enter a value");
+    } else if (passwordLength < 8 || passwordLength > 128) {
+        //second user validation is for if the value is less than 8 or greater than 128
+        passwordLength = parseInt(prompt("You must choose a number between 8 and 128."));
+    } else {
+        confirmNumber = confirm("Will this contain numbers?");
+        confirmCharacter = confirm("Will this contain special characters?")
+        confirmUppercase = confirm("Will this contain uppercase letters?")
+        confirmLowercase = confirm("Will this contain lowercase letters?")
+    };
+
+    //Verification to prevent all four options above being false
+    if (!confirmCharacter && !confirmNumber && !confirmUppercase && !confirmLowercase) {
+        choices = alert("You must select one of the options.  Please try again.");
+    }
+    // if statement assigns choices to a prompt verification, the four else if statements are for positive responses
+    else if (confirmCharacter && confirmNumber && confirmUppercase && confirmLowercase) {
+        choices = character.concat(number, alphaLower, alphaUpper)
+    }
+    // else if for four positive options
+    else if (confirmCharacter && confirmNumber && confirmUppercase && confirmLowercase) {
+        choices = character.concat(number, alphaLower, alphaUpper)
+    }
+    // else if for three positive options
+    else if (confirmCharacter && confirmNumber && confirmUppercase) {
+        choices = character.concat(number, alphaUpper);
+    }
+    else if (confirmCharacter && confirmNumber && confirmLowercase) {
+        choices = character.concat(number, alphaUpper);
+    }
+    else if (confirmCharacter && confirmLowercase && confirmUppercase) {
+        choices = character.concat(alphaLower, alphaUpper);
+    }
+    else if (confirmNumber && confirmLowercase && confirmUppercase) {
+        choices = number.concat(alphaLower, alphaUpper);
+    }
+    // Else if for two positive reponses
+    else if (confirmCharacter && confirmNumber) {
+        choices = character.concat(number);
+    }
+    else if (confirmCharacter && confirmLowercase) {
+        choices = character.concat(alphaLower);
+    }
+    else if (confirmCharacter && confirmUppercase) {
+        choices = character.concat(alphaUpper);
+    }
+    else if (confirmLowercase && confirmNumber) {
+        choices = alphaLower.concat(number);
+    }
+    else if (confirmLowercase && confirmUppercase) {
+        choices = alphaLower.concat(alphaUpper);
+    }
+    else if (confirmNumber && confirmUppercase) {
+        choices = number.concat(alphaUpper);
+    }
+    // Else if for one positive response
+    else if (confirmCharacter) {
+        choices = character;
+    }
+    else if (confirmNumber) {
+        choices = number;
+    }
+    else if (confirmLowercase) {
+        choices = alphaLower;
+    }
+    else if (confirmUppercase) {
+        choices = alphaUpper;
+    };
+    
 
 // // Assigning a length to the password from a prompt
 // var passwordLength = prompt('What would you like the length of the password to be? (Please enter a number between 8 and 120)');
@@ -52,79 +136,58 @@ passwordLength;
         //console.log("Your password length will be " + passwordLength + " characters long")
     };       
 
-// Lowercase letters prompt
-charTypesLowercase;
-if (charTypesLowercase) {
-    //console.log("You would like to use lowercase letters");
-    var lowercase = "abcdefghijklmnopqrstuvwxyz";
-} else {
-    //console.log("You do not want to use lowercase letters");
-    var lowercase = "";
-};
+// // Lowercase letters prompt
+// charTypesLowercase;
+// if (charTypesLowercase) {
+//     //console.log("You would like to use lowercase letters");
+//     var lowercase = "abcdefghijklmnopqrstuvwxyz";
+// } else {
+//     //console.log("You do not want to use lowercase letters");
+//     var lowercase = "";
+// };
 
-// Uppercase letters prompt
-charTypesUppercase;
-if (charTypesUppercase) {
-    //console.log("You would like to use uppercase letters");
-    var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-} else {
-    //console.log("You do not want to use uppercase letters");
-    var uppercase = "";
-};
+// // Uppercase letters prompt
+// charTypesUppercase;
+// if (charTypesUppercase) {
+//     //console.log("You would like to use uppercase letters");
+//     var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+// } else {
+//     //console.log("You do not want to use uppercase letters");
+//     var uppercase = "";
+// };
 
-// Numeric prompt
-charTypesNumeric;
-if (charTypesNumeric) {
-    //console.log("You would like to use numbers");
-    var numeric = "1234567890";
-} else {
-    //console.log("You do not want to use numbers");
-    var numeric = "";
-};
+// // Numeric prompt
+// charTypesNumeric;
+// if (charTypesNumeric) {
+//     //console.log("You would like to use numbers");
+//     var numeric = "1234567890";
+// } else {
+//     //console.log("You do not want to use numbers");
+//     var numeric = "";
+// };
 
-// Special Characters prompt
-charTypesSpecial;
-if (charTypesSpecial) {
-    //console.log("You would like to use special characters")
-    var special = "!@#$%^&*()_-=+,.?/<>"
-} else {
-    //console.log("You do not want to use special characters")
-    var special = "";
-};
+// // Special Characters prompt
+// charTypesSpecial;
+// if (charTypesSpecial) {
+//     //console.log("You would like to use special characters")
+//     var special = "!@#$%^&*()_-=+,.?/<>"
+// } else {
+//     //console.log("You do not want to use special characters")
+//     var special = "";
+// };
 
-// Validating that one of the input methods was chosen
-if (charTypesLowercase === false && charTypesUppercase === false && charTypesNumeric === false && charTypesSpecial === false) {
-    alert("You must select one of the options.");
-};
+// // Validating that one of the input methods was chosen
+// if (charTypesLowercase === false && charTypesUppercase === false && charTypesNumeric === false && charTypesSpecial === false) {
+//     alert("You must select one of the options.");
+// };
 
-// Adding the selected inputs to a single variable and console logging the options
-var chars = lowercase.concat(uppercase, numeric, special);
-//console.log("Your password will consist of the following characters: " + chars);
+// // Adding the selected inputs to a single variable and console logging the options
+// var chars = lowercase.concat(uppercase, numeric, special);
+// //console.log("Your password will consist of the following characters: " + chars);
 
-// ---PASSWORD---
-// Write password to the #password input
-function writePassword() {
-    var passwordText = document.querySelector("#password");
-    var password = generatePassword(); {
-    passwordText.value = password;
-    }
-};
 
-function generatePassword() {
-    //Asks for User Input
-    enter = parseInt(prompt("How many characters long would you like your password to be?  Please enter an amount between 8 and 128."));
-    //If statement for user validation, first statement is if the field is left blank
-    if (!enter) {
-        alert("You must enter a value");
-    } else if (enter < 8 || enter > 128) {
-        //second user validation is for if the value is less than 8 or greater than 128
-        enter = parseInt(prompt("You must choose a number between 8 and 128."));
-    } else {
-        confirmNumber = confirm("Will this contain numbers?");
-        confirmCharacter = confirm("Will this contain special characters?")
-        confirmUppercase = confirm("Will this contain uppercase letters?")
-        confirmLowercase = confirm("Will this contain lowercase letters?")
-    };
+
+
 
     //Verification to prevent all four options above being false
     if (!confirmCharacter && !confirmNumber && !confirmUppercase && !confirmLowercase) {
